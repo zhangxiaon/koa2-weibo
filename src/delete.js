5e145f722dab667d15f44905cfa67f5e30a992f8
zhangxiaon <webdeveloper.zhang@gmail.com>
2020-04-15 21:10:43 +0800

    seq

---
+++ src/delete.js
@@ -0,0 +1,12 @@
+const {User} = require("./model")
+
+!(async function () {
+    const updatezhangsan = await User.update({
+        nickName: "zhangsan1",
+    },{
+        where:{
+            userName:"zhangsan"
+        }
+    })
+    console.log("updatezhangsan",updatezhangsan)
+})()
